915e2ebd64a4e91be582008fe0c89d6343e0956c
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-08-26 17:17:34 +0600

    completed task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      num: "",
+      result: ""
+    };
+  }
+
+  handlenum = (event) => {
+    this.setState({
+      num: event.target.value,
+    });
+  };
+
+  exe = (event) => {
+    var n = parseInt(this.state.num);
+
+    function factorial(n) {
+      if (n > 0 && n <= 1) {
+        return 1;
+      } else {
+        return n * factorial(n - 1);
+      }
+    }
+
+    /* factorial(n); */
+
+    this.setState({
+      result: factorial(n)
+    });
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div style={{ margin: "50px" }}>
+        <h1>Factorial Calculator</h1>
+
+        <form /* onSubmit={this.exe} */>
+          <input
+            type="number"
+            placeholder="Enter a number..."
+            value={this.state.num}
+            onChange={this.handlenum}
+          />
+          <br />
+          <button /* type="submit" */ onClick={this.exe}>
+        
+            Calculate Factorial
+          </button>
+        </form>
+
+        <h2>Factorial: {this.state.result}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
